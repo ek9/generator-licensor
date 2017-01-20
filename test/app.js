f582d406c9d5c9382bde80b428f25583c824b5de
ek9 <dev@ek9.co>
2017-01-20 21:17:44 +0100

    add basic integration with generator-license and generator-license-cc

--- test/app.js
+++ test/app.js
@@ -3,16 +3,35 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-licenses:app', function () {
+var testName = 'Bob Jr.';
+var testEmail = 'bob@example.org';
+var testWebsite = 'https://www.example.org';
+var testYear = '2013-2015';
+var testTitle = 'Bob Jr. Media Files';
+var ccDir = 'media/';
+
+describe('licensor:app', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
+      .withPrompts({
+        hasSourceCode: true,
+        name: testName,
+        email: testEmail,
+        website: testWebsite,
+        year: testYear,
+        hasCreativeWork: true,
+        defaultLicense: 'MIT',
+        license: 'CC-BY-SA-4.0',
+        title: testTitle,
+        ccDir: 'media/',
+        someAnswer: true
+      }).toPromise();
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      'LICENSE',
+      'media/LICENSE',
     ]);
   });
 });
